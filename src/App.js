7b3d60fd50553c0f6c4411ed9f8a4be2145a2260
CatalinaMPA <catalina.pontieri.antelo@gmail.com>
2023-07-24 21:41:05 -0300

    cositas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import { SearchBar } from './components/SearchBar';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from './img/logo.png';
 
 /*
-Consultas:
-Problemas con la API
-1: La descripcion del comic siempre figura null... Que hacemos con eso?
-
+Consultas
 */
 
 
@@ -16,8 +13,24 @@ function App() {
   const [heroesSearchResults, setHeroesSearchResults] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
   const [lastHeroOrComicSearch, setLastHeroOrComicSearch] = useState('');
-  const key = '&ts=1925&apikey=1cbd647c852c32a1da3f922d7601548b&hash=628d798fa246cb3f5cc406fe721daea9';
+  const key = 'ts=1925&apikey=1cbd647c852c32a1da3f922d7601548b&hash=628d798fa246cb3f5cc406fe721daea9';
   const [comicURLResult, setComicURLResult] = useState([]);
+  const [heroesComicList, setHeroesComicList] = useState([]);
+  const [heroeComicOne, setHeroeComicOne] = useState('');
+
+  const [responseDataHeroe, setResponseDataHeroe] = useState([]);
+  const [responseDataComic, setResponseDataComic] = useState([]);
+
+  useEffect(() => {
+    // mostrar 8 heroes random
+    let offset = Math.floor(Math.random() * 1000);
+    fetch(`https://gateway.marvel.com:443/v1/public/characters?${key}&limit=8&offset=${offset}`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        setHeroesSearchResults(heroesSearchResults.concat(response.data.results));
+      })
+  }, [])
 
   function searchHeroOrComic(heroOrComic, offset) {
     if (heroOrComic === "") {
@@ -30,7 +43,7 @@ function App() {
       let endIndex = heroOrComic.indexOf('/', comicsDefaultLength);
       let comicId = heroOrComic.substring(comicsDefaultLength, endIndex);
       // extraer el comic id del comicURL
-      fetch(`http://gateway.marvel.com/v1/public/comics/${comicId}?${key}`)
+      fetch(`http://gateway.marvel.com/v1/public/comics/${comicId}?&${key}`)
         .then(response => response.json())
         .then(response => {
           console.log(comicId);
@@ -38,26 +51,35 @@ function App() {
           setComicURLResult(response.data.results);
           setHeroesSearchResults([]);
         });
+      document.getElementById('button-next-id').style.display = "none";
       return;
     }
 
+    let results;
+    function fetchHeroes() {
+    }
+    function fetchComics() {
+    }
     //--HEROES
     // buscamos los heroes
-    fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${heroOrComic}&offset=${offset * 4}&limit=4${key}`)
+    fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${heroOrComic}&offset=${offset * 4}&limit=4&${key}`)
       .then(response => response.json())
       .then(response => {
+        console.log(response);
         console.log(response.data.results);
+        results = response.data.results;
         // guardamos los resultados de la busqueda de heroes en la variable nueva results
-        let results = response.data.results;
+        setResponseDataHeroe(response.data);
         //--COMICS
         // buscamos los comics
-        fetch(`http://gateway.marvel.com/v1/public/comics?titleStartsWith=${heroOrComic}&offset=${offset * 4}&limit=4${key}`)
+        fetch(`http://gateway.marvel.com/v1/public/comics?titleStartsWith=${heroOrComic}&offset=${offset * 4}&limit=4&${key}`)
           .then(response => response.json())
           .then(response => {
+            console.log(response);
             console.log(response.data.results);
             // combinamos los resultados de la busqueda de comics con los de los herores en la variable results
             results = results.concat(response.data.results)
-            console.log(results);
+            setResponseDataComic(response.data);
             if (offset == 0) {
               // si el offset es 0 muestra los primeros 5 resultados de comics y heroes
               setHeroesSearchResults(results);
@@ -67,21 +89,40 @@ function App() {
               //con los resultados de la busqueda
               setHeroesSearchResults(heroesSearchResults.concat(results));
             }
+            document.getElementById('button-next-id').style.display = response.data.total ? "block" : "none";
           });
       });
   }
 
+  function fetchHeroesComicsList(comicId) {
+    fetch(`https://gateway.marvel.com:443/v1/public/characters/${comicId}/comics?&${key}&limit=1`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response.data.results);
+        setHeroesComicList(response.data.results);
+      });
+  }
+
+  // Fecha de Comic
   let comicDate;
   if (comicURLResult.length > 0) {
     comicDate = comicURLResult[0].dates[0].date;
-    comicDate = new Date(Date.parse(comicDate)).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"});
+    comicDate = new Date(Date.parse(comicDate)).toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric" });
   }
-  else {
-    comicDate = '';
+
+  // Descripcion de Comic
+  let comicDescription;
+  if (comicURLResult.length > 0) {
+    comicDescription = comicURLResult[0].description;
+    if (comicDescription == null) {
+      comicDescription = 'Este comic no tiene descripcion disponible.';
+    }
   }
 
   return (
-    <div className="App">
+    <div className="App" onLoad={() => {
+
+    }}>
       <header className="App-header">
         <img className='img-logo'
           src={logo}
@@ -96,16 +137,53 @@ function App() {
         }} />
       </header>
 
+      <div id='background-comic-list'>
+        <div className='comic-list'>
+          <div>
+            <p>{heroeComicOne}</p>
+            <button
+              onClick={() => {
+                document.getElementById('background-comic-list').style.display = "none";
+              }}>
+              X
+            </button>
+          </div>
+          <ul>
+            {
+              heroesComicList.map(comic => (
+                <li key={comic.id} className='list-comic'>
+                  {/* <p className='main-cardkasu-comic-description'>{comicDescription}</p> */}
+                  <img className='list-card-hero-img'
+                    src={comic.thumbnail.path + '.' + comic.thumbnail.extension}
+                  />
+                  <p className='list-card-comic-name'>{comic.title}</p>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+
       <ul className='main-cards' id='root'>
         {
           //Map recorre los elementos de heroesSearchResult, mostrando a los heroes.
           heroesSearchResults.map(item => (
 
             <li key={item.id}>
-              <p className='main-card-hero-name'>{item.name ? item.name : item.title}</p>
-              <img className='main-card-hero-img'
-                src={item.thumbnail.path + '.' + item.thumbnail.extension}
-              />
+              <button
+                onClick={() => {
+                  if (item.digitalId != undefined) {
+                    return;
+                  }
+                  document.getElementById('background-comic-list').style.display = "block";
+                  fetchHeroesComicsList(item.id);
+                  setHeroeComicOne(item.name);
+                }}>
+                <p className='main-card-hero-name'>{item.name ? item.name : item.title}</p>
+                <img className='main-card-hero-img'
+                  src={item.thumbnail.path + '.' + item.thumbnail.extension}
+                />
+              </button>
             </li>
           ))
         }
@@ -114,10 +192,11 @@ function App() {
       {/* boton de siguiente */}
       {/* habria que cambiarlo que no se un boton y que pase cuando scrolleas */}
       {/* o esconderlo cuando solo se muestra un comic */}
-      <button onClick={() => {
-        setPageNumber(pageNumber + 1);
-        searchHeroOrComic(lastHeroOrComicSearch, pageNumber + 1)
-      }}>Siguiente</button>
+      <button className='button-next' id='button-next-id'
+        onClick={() => {
+          setPageNumber(pageNumber + 1);
+          searchHeroOrComic(lastHeroOrComicSearch, pageNumber + 1)
+        }}>Siguiente</button>
 
       <ul>
         {
@@ -130,7 +209,7 @@ function App() {
                   <p key={creator.role + Math.random()} className='main-card-comic-creators'>{creator.role}: {creator.name}</p>
                 ))
               }
-              <p className='main-cardkasu-comic-description'>{comic.description}</p>
+              <p className='main-cardkasu-comic-description'>{comicDescription}</p>
               <img className='main-card-hero-img'
                 src={comic.thumbnail.path + '.' + comic.thumbnail.extension}
               />
